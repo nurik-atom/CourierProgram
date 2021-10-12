5309e37fdbd8e811012d9124341d0c6bfb14e357
nurik-atom <baliikhanov@gmail.com>
2021-10-12 17:46:53 +0500

    update users table

--- database/migrations/2021_10_12_164452_add_new_to_users.php
+++ database/migrations/2021_10_12_164452_add_new_to_users.php
@@ -16,7 +16,6 @@ class AddNewToUsers extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->string("birthday")->after("id_city")->nullable();
             $table->string("photo")->after("birthday")->nullable();
-            $table->renameColumn("type", "type_transport");
         });
     }
 
@@ -30,7 +29,6 @@ class AddNewToUsers extends Migration
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn("birthday");
             $table->dropColumn("photo");
-            $table->renameColumn("type_transport", "type");
         });
     }
 }
