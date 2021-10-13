740adbcde61a78a90281b637d0f889c895ded8b3
nurik-atom <baliikhanov@gmail.com>
2021-10-13 17:32:46 +0500

    getStatusUser added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -153,7 +153,7 @@ class UserController extends Controller
                 break;
             }
 
-            $user_data_update = DB::table("users")->where([["password" ,'=', $password], ['phone','=',$phone]])->update(['name' => $name, 'surname' => $surname,'id_city'=>$id_city,'birthday'=>$birthday,  'type' => $type_transport]);
+            $user_data_update = DB::table("users")->where([["password" ,'=', $password], ['phone','=',$phone]])->update(['name' => $name, 'surname' => $surname,'id_city'=>$id_city,'birthday'=>$birthday,  'type' => $type_transport, 'status'=>'2']);
 
             if ($user_data_update)  $data['success'] = true;
 
