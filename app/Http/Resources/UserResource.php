d07520528ea23a07aab092b6e03853990e4898b4
nurik-atom <baliikhanov@gmail.com>
2021-11-02 14:18:55 +0500

    new Functions

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
+
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+
+    public function toArray($request)
+    {
+        $types = array(1=>'Пешый курьер', 2=>'Вело курьер', 3=>'Мото курьер', 4=>'Авто курьер');
+        return [
+            'id'=>$this->id,
+            'name'=>$this->name,
+            'surname'=>$this->surname,
+            'birthday'=>$this->birthday,
+            'photo'=>$this->photo ? $this->photo : null,
+            'phone'=>"+".$this->phone,
+            'status'=>$this->status,
+            'type'=>$this->type,
+            'type_text'=>($this->type ? $types[$this->type] : ""),
+            'state'=>$this->state,
+            'rating'=>$this->rating
+        ];
+    }
+}
