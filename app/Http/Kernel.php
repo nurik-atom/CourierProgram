a231c10a0ec91bf7d81c7eacf2280a1babca9454
nurik-atom <baliikhanov@gmail.com>
2022-04-18 15:10:39 +0500

    Kernel.php 17.04

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,7 +37,12 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ]
+        ],
+
+        'api' => [
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
     ];
 
     /**
