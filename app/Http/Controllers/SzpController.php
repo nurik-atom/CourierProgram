db397097ae87c9e644b8b37e4b2be6d7a83bf77a
nurik-atom <baliikhanov@gmail.com>
2023-01-26 11:29:19 +0500

    statisticaOrders

--- app/Http/Controllers/SzpController.php
+++ app/Http/Controllers/SzpController.php
@@ -900,12 +900,14 @@ class SzpController extends Controller
                 ->selectRaw("COUNT(*) as kol, SUM(amount) as summa, type")
                 ->whereRaw("DATE(created_at) >= '$start_date' AND DATE(created_at) <= '$end_date' AND amount > 0")
                 ->groupBy('type')
+                ->pluck('type')
                 ->get();
 
             $result['compare_balance_plus'] = DB::table('balance_history')
                 ->selectRaw("COUNT(*) as kol, SUM(amount) as summa, type")
                 ->whereRaw("DATE(created_at) >= '$compare_start_date' AND DATE(created_at) <= '$compare_end_date' AND amount > 0")
                 ->groupBy('type')
+                ->pluck('type')
                 ->get();
 
             $result['cash_change'] = DB::table('cash_driver_history')
