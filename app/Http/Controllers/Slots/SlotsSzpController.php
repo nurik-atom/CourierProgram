b4f0a4c08b5edb9559e2fa7afe208a35714b0b7c
nurik-atom <baliikhanov@gmail.com>
2024-04-18 18:40:35 +0500

    Table Slots SlotsSzpController starts

---
+++ app/Http/Controllers/Slots/SlotsSzpController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Slots;
+
+use App\Http\Controllers\Controller;
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SlotsSzpController extends Controller
+{
+    public function updateOrCreateSlotsFromSzp(Request $request){
+        $key        = $request->input("key");
+        $date_day   = $request->input("date_day");
+        $hour       = $request->input("hour");
+        $kol        = $request->input("kol");
+        $id_admin   = $request->input("id_admin");
+        $result['success'] = false;
+
+        if (!$key || $key != env("ALLFOOD_KEY")) exit("Error key");
+
+        do{
+
+            $updateOrInsert = DB::table('slots')->updateOrInsert(
+                [   'date_day'  => $date_day,
+                    'hour'      => $hour
+                ],
+                [
+                'status'    => 1,
+                'kol'       => $kol,
+                'id_admin'  => $id_admin,
+                'updated_at' => Carbon::now(),
+            ]);
+
+            if (!$updateOrInsert) break;
+
+            $id_new_slot = DB::table('slots')->select('id')
+                ->where('date_day',$date_day)
+                ->where('hour', $hour)
+                ->first();
+
+            $result['success'] = true;
+            $result['id_new_slot'] = $id_new_slot->id;
+
+        }while(false);
+
+        return response()->json($result);
+    }
+
+    public function getSlotsFromSzp(Request $request){
+        $key  = $request->input("key");
+        $from = $request->input("from");
+        $to   = $request->input("to");
+        $result['success'] = false;
+
+        do{
+
+            $slots = DB::table('slots')
+                ->where('date_day', '>=', $from)
+                ->where('date_day', '<=', $to)
+                ->get();
+
+            if (!$slots) break;
+
+            $result['success'] = true;
+            $result['slots'] = $slots;
+        }while(false);
+
+        return response()->json($result);
+    }
+
+    public function unSubSlotsFromSzp(Request $request){
+
+    }
+}
