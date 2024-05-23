ae6c0e954eb6c27af28c3258fb3541a8d85c0744
nurik-atom <baliikhanov@gmail.com>
2024-05-23 15:38:26 +0500

    Telescope AdminMiddleware

--- app/Models/Admins.php
+++ app/Models/Admins.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admins extends Model
+class Admins  extends Authenticatable
 {
     protected $table = "admins";
 }
